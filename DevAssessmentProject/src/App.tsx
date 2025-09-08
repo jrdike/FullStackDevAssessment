c5c90fbd47636bbb90394e93092dc6da3668aed8
jrdike <jackson.r.dike@gmail.com>
2025-09-08 18:30:51 -0400

    Basic Vite setup

---
+++ DevAssessmentProject/src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="cards">
+        <button id="1" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        
+      </div>
+      <div className="buttons">
+        <button id="sort1" onClick={() => setCount(0)}>
+          Sort by most clicked
+        </button>
+        <button id="sort2" onClick={() => setCount(0)}>
+          Sort by earliest clicked
+        </button>
+
+        <br></br>
+
+        <button id="clear" onClick={() => setCount(0)}>
+          Clear
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
