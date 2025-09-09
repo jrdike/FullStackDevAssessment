d46d9100c257e2883b869444630e847389176e90
jrdike <jackson.r.dike@gmail.com>
2025-09-09 00:54:34 -0400

    Added button functionality

--- DevAssessmentProject/src/App.tsx
+++ DevAssessmentProject/src/App.tsx
@@ -1,66 +1,81 @@
+import { dummyCards } from './data/devcard'
+import { useState } from 'react'
 import type { Devcard } from './types/devcard'
-
-const cards: Devcard[] = []
-for (let i = 1; i <= 8; i++) {
-    cards.push({
-      id: i.toString(),
-      clicks: 0,
-      timestamp: null
-  })
-}
 
 //I used this from https://flowbite.com/docs/components/buttons/ (specifically the one labelled alternative) as I'm not the best with styling
 const defaultButtonCSS: string = "py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-
-function splitCards() {
-  const highCards: Devcard[] = []
-  const lowCards: Devcard[] = []
-  for (let i = 0; i < cards.length / 2; i++) {
-    highCards.push(cards[i])
-    lowCards.push(cards[i + cards.length / 2])
-  }
-  return [highCards, lowCards]
-}
+//Modified CSS for main buttons
+const mainButtonCSS: string = "w-1/5 h-48 align-middle py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
 
 function App() {
-  let split: Devcard[][] = splitCards()
-  let highCards: Devcard[] = split[0]
-  let lowCards: Devcard[] = split[1]
+  const [cards, setCards] = useState(dummyCards)
   return (
     <>
       <div className="py-10 text-center">
-        {highCards.map(card => (
-          <button id={card.id} className={defaultButtonCSS}>
-            <h3 className="">{card.id}</h3>
-            <p>Clicks: {card.clicks}</p>
-            <p>{card.timestamp == null ? "Never Clicked" : card.timestamp}</p>
-          </button>
-        ))}
-        <br></br>
-        {lowCards.map(card => (
-          <button id={card.id} className={defaultButtonCSS}>
-            <h3 className="">{card.id}</h3>
-            <p>Clicks: {card.clicks}</p>
-            <p>{card.timestamp == null ? "Never Clicked" : card.timestamp}</p>
+        {cards.map(card => (
+          <button id={card.id} className={mainButtonCSS} onClick = {() => setCards((cards) => 
+          (
+            cards.map(c => (c.id === card.id ? {
+              id: card.id,
+              clicks: card.clicks + 1,
+              timestamp: card.timestamp == null ? new Date() : card.timestamp
+            } : c))
+          ))}>
+            <h3 className="font-bold text-3xl">{card.id}</h3>
+            <p className="align-bottom">Clicks: {card.clicks}</p>
+            <p className="align-bottom">{card.timestamp == null ? "Never Clicked" : card.timestamp.toUTCString()}</p>
           </button>
         ))}
       </div>
+
       <div className="text-center">
-        <button id="sort1" className={defaultButtonCSS}>
+        <button id="sort1" className={defaultButtonCSS} onClick = {() => {
+          sortCards(cards, true)
+          setCards((cards) => 
+          (
+            cards.map(card => ({
+              id: card.id,
+              clicks: card.clicks,
+              timestamp: card.timestamp
+            }))
+          ))}}>
           Sort by most clicked
         </button>
-        <button id="sort2" className={defaultButtonCSS}>
+        <button id="sort2" className={defaultButtonCSS} onClick = {() => {
+          sortCards(cards, false)
+          setCards((cards) => 
+          (
+            cards.map(card => ({
+              id: card.id,
+              clicks: card.clicks,
+              timestamp: card.timestamp
+            }))
+          ))}}>
           Sort by earliest clicked
         </button>
 
         <br></br>
 
-        <button id="clear" className={defaultButtonCSS}>
+        <button id="clear" className={defaultButtonCSS} onClick = {() => setCards((cards) => dummyCards)}>
           Clear
         </button>
       </div>
     </>
   )
+}
+
+function sortCards(cards: Devcard[], click: boolean) {
+  cards.sort((a: Devcard, b: Devcard) => {
+    if (click) {
+      return b.clicks - a.clicks
+    } else {
+      let a_time = a.timestamp
+      let b_time = b.timestamp
+      if (b_time == null) return -1
+      else if (a_time == null) return 1
+      else return a_time.getTime() - b_time.getTime()
+    }
+  })
 }
 
 export default App
